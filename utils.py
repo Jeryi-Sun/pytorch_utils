5853aac2a6f53a01e1b12a40cb542d123d6068f2
eryihaha <18231535@bjtu.edu.cn>
2021-10-03 13:44:21 +0800

    add averageMeter

--- utils.py
+++ utils.py
@@ -2,6 +2,12 @@ import torch
 import torch.nn as nn
 from torch.nn import functional as F
 class SmoothCrossEntropy(nn.Module):
+    """
+    loss = SmoothCrossEntropy()
+    input = torch.randn(3, 5, requires_grad=True)
+    target = torch.empty(3, dtype=torch.long).random_(5)
+    output = loss(input, target)
+    """
     def __init__(self, alpha=0.1):
         super(SmoothCrossEntropy, self).__init__()
         self.alpha = alpha
@@ -14,9 +20,30 @@ class SmoothCrossEntropy(nn.Module):
         loss = -(target_probs * torch.log_softmax(logits, dim=-1)).sum(dim=-1)
         return loss.mean()
 
+
+class AverageMeter(object):
+    """Computes and stores the average and current value"""
+    def __init__(self, name, fmt=':f'):
+        self.name = name
+        self.fmt = fmt
+        self.reset()
+
+    def reset(self):
+        self.val = 0
+        self.avg = 0
+        self.sum = 0
+        self.count = 0
+
+    def update(self, val, n=1):
+        self.val = val
+        self.sum += val * n
+        self.count += n
+        self.avg = self.sum / self.count
+
+    def __str__(self):
+        fmtstr = '{name} {val' + self.fmt + '} ({avg' + self.fmt + '})'
+        return fmtstr.format(**self.__dict__)
+
 if __name__ == '__main__':
-    loss = SmoothCrossEntropy()
-    input = torch.randn(3, 5, requires_grad=True)
-    target = torch.empty(3, dtype=torch.long).random_(5)
-    output = loss(input, target)
-    print(output)
+
+    print("hello")
